2e4c811ea6f7edc4dfcde9e277cb0a6acefa40b2
Mugiwara <kaan.sacma@epitech.eu>
2022-06-14 18:26:23 +0200

    Added an old realisation in 2018

---
+++ conf.lua
@@ -0,0 +1,4 @@
+function love.conf(t)   
+  t.window.title = "Failed Guitar No Hero" -- Change le titre de la fenêtre
+  t.window.icon = "images/icon.png" -- Change l'icone de la fenêtre
+end
---
+++ constants.lua
@@ -0,0 +1,2 @@
+TITLE = "Failed Guitar No Hero" -- Titre
+PATH_ICON = "images/icon.png" -- Chemin image icône
